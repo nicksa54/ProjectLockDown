fcefb7ff92b9dc1f323f20058c201654621dc9c6
Nirmal Kumawat <kumawat@adobe.com>
2020-05-29 20:16:54 +0530

    Adding GameManager.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import GameManager from './src/components/GameManager';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => GameManager);
